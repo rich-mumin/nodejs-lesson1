cdf663d554a575235d94a491637dee71c12e035c
rich-mumin <muslim1nureke@gmail.com>
2023-12-12 20:25:51 +0600

    added

---
+++ lesson1/server.js
@@ -0,0 +1,25 @@
+const { log } = require('console');
+const os = require('os');
+const path = require('path'); 
+const { add, multiply,bolu  } = require('./math');
+
+log(add(1,1));
+log(multiply(2, 3));
+log(bolu(9, 3));
+// log(os.type());
+// log(os.version());
+// log(os.homedir());
+// log(os.machine());
+
+// log(__dirname);
+// log(__filename);
+
+// log(path.basename(__filename));
+// log(path.dirname(__filename));
+// log(path.extname(__filename));
+
+// log(path.parse(__filename));
+
+
+
+
